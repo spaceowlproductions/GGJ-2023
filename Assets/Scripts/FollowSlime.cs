f060c39f5fc81033d5ff6c1a89563b0b58057f38
noahneras <110417604+noahneras@users.noreply.github.com>
2023-02-05 13:21:32 -0500

    Revert "Revert "Asset Dump + AudioMaster Change""
    This reverts commit b8692aa00017ba5f43ea471dc4edf4c2d6e93fac.

--- Assets/Scripts/FollowSlime.cs
+++ Assets/Scripts/FollowSlime.cs
@@ -17,6 +17,8 @@ public class FollowSlime : MonsterController
 
         audioSource.clip = AudioController.moveClips[Random.Range(0, AudioController.moveClips.Length)];
         audioSource.Play();
+        audioSource.clip = AudioController.lungeClips[Random.Range(0, AudioController.moveClips.Length)];
+        audioSource.Play();
 
         StartCoroutine(AttackWait());
 
